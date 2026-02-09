1cb2c08e9ac89c7c0fd8470e25df3c71f795f8e9
nat-kea <nafo@kea.dk>
2026-02-09 09:38:41 +0100

    Update espnow_sender.py

--- espnow_sender.py
+++ espnow_sender.py
@@ -11,7 +11,8 @@ esp_now.active(True)
 peer = b'\xff\xff\xff\xff\xff\xff'     # MAC address of peer's wifi interface
 esp_now.add_peer(peer)                 # Must add_peer() before send()
 
-esp_now.send(peer, "Starting...")
+esp_now.send(peer, b"Starting...")
 for i in range(100):
     esp_now.send(peer, str(i)*20, True)
+
 esp_now.send(peer, b'end')
