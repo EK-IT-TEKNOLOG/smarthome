22572a36e70ac0ab37ea65c74d08e2ddfcb9fd0d
nat-kea <nafo@kea.dk>
2026-02-16 11:56:59 +0100

    Update smarthome_sensor_hcsr04.py

--- smarthome_sensor_hcsr04.py
+++ smarthome_sensor_hcsr04.py
@@ -74,7 +74,7 @@ while True:
 
     # Send data if there is a change (this principle saves power)
     if bat_pct != prev_bat_pct or sensor_value != prev_sensor_value:
-        data_string = str(time.ticks_ms()) + '|' + str(bat_pct) + '|' + str(sensor_value) # The data to send. CHANGE IT!
+        data_string = 'JERES NAVN' + '|' + str(time.ticks_ms()) + '|' + str(bat_pct) + '|' + str(sensor_value) # The data to send. CHANGE IT!
         
         print("Sending: " + data_string)
         try:
@@ -87,3 +87,4 @@ while True:
         prev_sensor_value = sensor_value
     
     time.sleep_ms(20)                  # Needed to avoid spurious measurements. Min 20 ms
+
